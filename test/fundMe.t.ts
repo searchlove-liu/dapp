506aa88ef16d887c0ada6c80edcc1bd2b4939989
lcl <15535435950@163.com>
2025-09-10 22:02:28 +0800

    migration to linux

--- test/fundMe.t.ts
+++ test/fundMe.t.ts
@@ -11,4 +11,11 @@ describe("test fundMe contract", async function () {
         await fundMe.deploymentTransaction()?.wait(1);
         assert.equal((await fundMe.owner()), accounts[0].address)
     })
+
+    it("test if dataFeed is assigned correctly", async function () {
+        const fundMe = await ethers.deployContract("FundMe", [180n]);
+        await fundMe.deploymentTransaction()?.wait(1);
+        const dataFeed = await fundMe.dataFeed();
+        assert.equal(dataFeed, "0x694AA1769357215DE4FAC081bf1f309aDC325306");
+    })
 }) 
